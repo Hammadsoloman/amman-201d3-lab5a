b05fdbaa2f3872a8b48b7f81f33b40088e5a4fbc
h@soloman <coding202020@gmail>
2020-02-20 23:18:29 +0200

    first update for the problem1

--- tests.js
+++ tests.js
@@ -4,9 +4,9 @@
 
 function testSum() { //eslint-disable-line
   if (sum(4,7)[0] === 11 && sum(4,7)[1] === 'The sum of 4 and 7 is 11.') {
-    console.log('%c TEST FOR sum() PASSES', 'color: green');
+   console.log('%c TEST FOR sum() PASSES', 'color: green');
   } else {
-    console.log('%c TEST FOR sum() FAILS', 'color: red');
+ console.log('%c TEST FOR sum() FAILS', 'color: red');
   }
 }
 
